92165facaa013b0456c683fe4e420e52beef45c4
Nanda-nandu <116694884+Nanda-nandu@users.noreply.github.com>
2023-02-17 12:27:07 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.22.233:9000/',
+  serverUrl: 'http://18.179.222.49:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
